058a42303639b9053135f6b356fd802061d60ed6
jcstill <jacobcstill@gmail.com>
2019-11-22 18:38:10 -0800

    Update Main.c

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 //Pointer to the framebuffer memory.
 uint8_t *fbmem;
 
-#define FB_WIDTH 480
+#define FB_WIDTH 512
 #define FB_HEIGHT 320
 
 #define STUPID_OFFW 16
@@ -26,26 +26,15 @@ uint8_t *fbmem;
 								(((R) & 0xFF) <<  0))
 
 void setupscreen(){
-	GFX_REG(GFX_BGNDCOL_REG)=0;				// set bg to Bleck
-	GFX_REG(GFX_LAYEREN_REG)=0;				// Disable all gfx layers
 	fbmem=calloc(FB_WIDTH,FB_HEIGHT);		// Allocate screen buffer
-	GFX_REG(GFX_FBPITCH_REG)=(GFX_FBPITCH_PAL_OFF)|(FB_WIDTH<<GFX_FBPITCH_PITCH_OFF);	// Declare Pitch for the color Palette
-	GFX_REG(GFX_FBADDR_REG)=((uint32_t)fbmem);
-	for(int i=0; i<((FB_WIDTH+STUPID_OFFW) * FB_HEIGHT); i++){
-		fbmem[i] = 0;						// Clear the framebuffer
-	}
-	cache_flush(fbmem, fbmem+FB_WIDTH*FB_HEIGHT);
-	// GFX_REG(GFX_LAYEREN_REG)=GFX_LAYEREN_FB_8BIT|GFX_LAYEREN_FB|GFX_LAYEREN_TILEA;
-	// GFX_REG(GFX_LAYEREN_REG)=GFX_LAYEREN_FB_8BIT|GFX_LAYEREN_FB|GFX_LAYEREN_TILEA;	// Emable framebuffer and layer A
+	GFX_REG(GFX_FBADDR_REG)=((uint32_t)fbmem)&0xFFFFFF;
+	GFX_REG(GFX_FBPITCH_REG)=(256<<GFX_FBPITCH_PAL_OFF)|(512<<GFX_FBPITCH_PITCH_OFF);	// Declare Pitch for the color Palette
 	GFX_REG(GFX_LAYEREN_REG)=GFX_LAYEREN_FB_8BIT|GFX_LAYEREN_FB;	// Emable framebuffer
+	cache_flush(fbmem, fbmem+FB_WIDTH*FB_HEIGHT);
 }
 void setcolors(){
-	for(int i=0; i<256; i++){
-		GFXPAL[i] = COMP_COLOR(0xFF, i, i, i);
-	}
-	// GFXPAL[0] = COMP_COLOR(0xFF, 0x00, 0x00, 0x00);
-	// GFXPAL[1] = COMP_COLOR(0xFF, 0xFF, 0xFF, 0xFF);
-	// GFXPAL[0] = COMP_COLOR(0xFF, 0, 0, 0xFF);
+	GFXPAL[0] = COMP_COLOR(0xFF, 0, 0, 0);
+	GFXPAL[10] = COMP_COLOR(0xFF, 0xFF, 0xFF, 0xFF);
 }
 const uint8_t b_m16_interleave[] = { 0, 49, 49, 41, 90, 27, 117, 10 };
 uint8_t sin8_C( uint8_t theta){
@@ -77,122 +66,124 @@ uint8_t cos8_C( uint8_t theta){
     return sin8_C(theta + 64);
 }
 
+extern uint32_t GFXCOPPEROPS[];
+
 void main(int argc, char **argv){
 	setupscreen();
 	setcolors();
 	
-	float playerX=2.0;
-	float playerY=3.0;
-	float playerA=0.0;
+	// float playerX=2.0;
+	// float playerY=3.0;
+	// float playerA=0.0;
 	
-	float FOV=3.1415/2;
-	uint8_t depth=0.0;
-	if(MAP_WIDTH>MAP_HEIGHT){
-		depth=MAP_WIDTH;
-	}else{
-		depth=MAP_HEIGHT;
-	}
-	float rayA;
-	float distowal;
-	uint8_t hitwall;
-	float eyeX;
-	float eyeY;
-	int tmpX;
-	int tmpY;
-	int ceil;
-	int floor;
+	// float FOV=3.1415/2;
+	// uint8_t depth=0.0;
+	// if(MAP_WIDTH>MAP_HEIGHT){
+		// depth=MAP_WIDTH;
+	// }else{
+		// depth=MAP_HEIGHT;
+	// }
+	// float rayA;
+	// float distowal;
+	// uint8_t hitwall;
+	// float eyeX;
+	// float eyeY;
+	// int tmpX;
+	// int tmpY;
+	// int ceil;
+	// int floor;
 	
-	int gamemap[MAP_WIDTH][MAP_HEIGHT] = {
-		{1, 1, 1, 1, 1, 1, 1, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 1, 1, 1, 1, 1, 1, 1}
-	};
+	// int gamemap[MAP_WIDTH][MAP_HEIGHT] = {
+		// {1, 1, 1, 1, 1, 1, 1, 1},
+		// {1, 0, 0, 0, 0, 0, 0, 1},
+		// {1, 0, 0, 0, 0, 0, 0, 1},
+		// {1, 0, 0, 0, 0, 0, 0, 1},
+		// {1, 0, 0, 0, 0, 0, 0, 1},
+		// {1, 0, 0, 0, 0, 0, 0, 1},
+		// {1, 0, 0, 0, 0, 0, 0, 1},
+		// {1, 0, 0, 0, 0, 0, 0, 1},
+		// {1, 0, 0, 0, 0, 0, 0, 1},
+		// {1, 0, 0, 0, 0, 0, 0, 1},
+		// {1, 0, 0, 0, 0, 0, 0, 1},
+		// {1, 0, 0, 0, 0, 0, 0, 1},
+		// {1, 0, 0, 0, 0, 0, 0, 1},
+		// {1, 0, 0, 0, 0, 0, 0, 1},
+		// {1, 0, 0, 0, 0, 0, 0, 1},
+		// {1, 1, 1, 1, 1, 1, 1, 1}
+	// };
 	
 	// Main Game Loop
 	int run = 0;
-	while(run == 0){
+	// while(run == 0){
 		
-		if(MISC_REG(MISC_BTN_REG) & (BUTTON_LEFT)){
-			playerA -= 0.1;
-    	}
-		if(MISC_REG(MISC_BTN_REG) & (BUTTON_RIGHT)){
-			playerA += 0.1;
-    	}
+		// if(MISC_REG(MISC_BTN_REG) & (BUTTON_LEFT)){
+			// playerA -= 0.1;
+    	// }
+		// if(MISC_REG(MISC_BTN_REG) & (BUTTON_RIGHT)){
+			// playerA += 0.1;
+    	// }
 		
-		if(MISC_REG(MISC_BTN_REG) & (BUTTON_UP)){
-			playerX -= ((sin8_C(playerA*40+0x7F)-0x7F)/128.0) * 5;
-			playerY -= ((cos8_C(playerA*40+0x7F)-0x7F)/128.0) * 5;
-    	}
-		if(MISC_REG(MISC_BTN_REG) & (BUTTON_DOWN)){
-			playerX += ((sin8_C(playerA*40+0x7F)-0x7F)/128.0) * 5;
-			playerY += ((cos8_C(playerA*40+0x7F)-0x7F)/128.0) * 5;
-    	}
+		// if(MISC_REG(MISC_BTN_REG) & (BUTTON_UP)){
+			// playerX -= ((sin8_C(playerA*40+0x7F)-0x7F)/128.0) * 5;
+			// playerY -= ((cos8_C(playerA*40+0x7F)-0x7F)/128.0) * 5;
+    	// }
+		// if(MISC_REG(MISC_BTN_REG) & (BUTTON_DOWN)){
+			// playerX += ((sin8_C(playerA*40+0x7F)-0x7F)/128.0) * 5;
+			// playerY += ((cos8_C(playerA*40+0x7F)-0x7F)/128.0) * 5;
+    	// }
 
 		
 		
-		for(int screenW=0; screenW<FB_WIDTH+STUPID_OFFW; screenW++){
-			rayA=(playerA-FOV/2)+(screenW/(FB_WIDTH+STUPID_OFFW))*FOV;
+		// for(int screenW=0; screenW<FB_WIDTH+STUPID_OFFW; screenW++){
+			// rayA=(playerA-FOV/2)+(screenW/(FB_WIDTH+STUPID_OFFW))*FOV;
 			
 			
-			distowal=0.0;
-			hitwall=0;
+			// distowal=0.0;
+			// hitwall=0;
 			
-			eyeX=sin(rayA);
-			eyeY=cos(rayA);
+			// eyeX=sin(rayA);
+			// eyeY=cos(rayA);
 
 			// eyeX=(sin8_C(rayA*40+0x7F)-0x7F)/128.0;
 			// eyeY=(cos8_C(rayA*40+0x7F)-0x7F)/128.0;
 			
-			while(hitwall!=1 && distowal<depth){
-				distowal += 0.1;
+			// while(hitwall!=1 && distowal<depth){
+				// distowal += 0.1;
 				
-				tmpX=(playerX+eyeX*distowal);
-				tmpY=(playerY+eyeY*distowal);
+				// tmpX=(playerX+eyeX*distowal);
+				// tmpY=(playerY+eyeY*distowal);
 				
-				if(tmpX<0 || tmpX>=MAP_WIDTH || tmpY<0 || tmpY>=MAP_HEIGHT){
-					hitwall=1;
-					distowal=depth;
-				}else{
-					if(gamemap[tmpX][tmpY]==1){
-						hitwall=1;
-					}
-				}
-			}
+				// if(tmpX<0 || tmpX>=MAP_WIDTH || tmpY<0 || tmpY>=MAP_HEIGHT){
+					// hitwall=1;
+					// distowal=depth;
+				// }else{
+					// if(gamemap[tmpX][tmpY]==1){
+						// hitwall=1;
+					// }
+				// }
+			// }
 			
-			ceil = (FB_HEIGHT/2)-(FB_HEIGHT/distowal/2);
-			floor = FB_HEIGHT - ceil;
+			// ceil = (FB_HEIGHT/2)-(FB_HEIGHT/distowal/2);
+			// floor = FB_HEIGHT - ceil;
 			
 			
 			
-			for(int screenH = 0; screenH < FB_HEIGHT+STUPID_OFFH; screenH++){
+			// for(int screenH = 0; screenH < FB_HEIGHT+STUPID_OFFH; screenH++){
 				
-				if(screenH <= ceil){
-					fbmem[(screenH*(FB_WIDTH+STUPID_OFFW))+screenW] = 0;
-				}else if(screenH > ceil && screenH < floor){
-					fbmem[(screenH*(FB_WIDTH+STUPID_OFFW))+screenW] = 255 - (distowal * 15);
-				}else if(screenH >= floor){
-					fbmem[(screenH*(FB_WIDTH+STUPID_OFFW))+screenW] = 0;
-				}
-			}
-		}
-		cache_flush(fbmem, fbmem+FB_WIDTH*FB_HEIGHT);
-		if(MISC_REG(MISC_BTN_REG) & (BUTTON_START)){
-			run = 1;
-    	}
-	}
+				// if(screenH <= ceil){
+					// fbmem[(screenH*(FB_WIDTH+STUPID_OFFW))+screenW] = 0;
+				// }else if(screenH > ceil && screenH < floor){
+					// fbmem[(screenH*(FB_WIDTH+STUPID_OFFW))+screenW] = 255 - (distowal * 15);
+				// }else if(screenH >= floor){
+					// fbmem[(screenH*(FB_WIDTH+STUPID_OFFW))+screenW] = 0;
+				// }
+			// }
+		// }
+		// cache_flush(fbmem, fbmem+FB_WIDTH*FB_HEIGHT);
+		// if(MISC_REG(MISC_BTN_REG) & (BUTTON_START)){
+			// run = 1;
+    	// }
+	// }
 	
 		// for(int i = 0; i< 100; i++){
 			// for(int h = 0; h<FB_HEIGHT+STUPID_OFFH; h++){
@@ -221,11 +212,44 @@ void main(int argc, char **argv){
 		
 		
 		// cache_flush(fbmem, fbmem+FB_WIDTH*FB_HEIGHT);
-	// while(run == 0){
-		// if(MISC_REG(MISC_BTN_REG) & (BUTTON_START)){
-			// run = 1;
-		// }
-	// }
+	
+	
+	
+	
+	
+	
+	int bgnd_pal_state=0;
+	uint32_t *cmem=GFXCOPPEROPS;
+		GFX_REG(GFX_COPPER_CTL_REG)=GFX_COPPER_PC_OFF;
+		for(int high = 0; high<FB_WIDTH; high++){
+			for(int wid = 0; wid<FB_WIDTH; wid++){			
+				*cmem++=COPPER_OP_WAIT(wid, high);
+				*cmem++=COPPER_OP_WRITE(&GFX_REG(GFX_COPPER_PC_OFF), 1);
+				*cmem++=10;
+			}
+		}
+		//Last instruction: loop back to start
+		*cmem++=COPPER_OP_RESET;
+		//Enable copper so it can... errm... cop.
+		GFX_REG(GFX_COPPER_CTL_REG)=GFX_COPPER_CTL_RUN;
+	while(run == 0){
+	
+
+	
+	
+	for(int i=0; i<100000000; i++){
+		
+	}
+	
+	
+	
+	
+	
+	
+		if(MISC_REG(MISC_BTN_REG) & (BUTTON_START)){
+			run = 1;
+		}
+	}
 	
 	
 }
